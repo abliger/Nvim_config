0b91ffb6cbba50b0cdda1f14293da22af3c27099
abliger <3330181534@qq.com>
2022-06-23 23:27:38 +0800

    add kb-switch

--- init.lua
+++ init.lua
@@ -1,3 +1,41 @@
-local api=vim.api
-api.nvim_command('set number')
+local fn = vim.fn
+local api = vim.api
+
+is_mac = fn.has("mac") or fn.has("unix") or fn.has('linux')
+local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {
+  clear = true,
+})
+api.nvim_command('set nu')
 api.nvim_command('set mouse=a')
+api.nvim_command('command -nargs=? OpenConfig edit ' .. fn.stdpath('config') .. '/<args>')
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local paccker_bootstrap
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.notify("正在安装Pakcer.nvim，请稍后...")
+  paccker_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    -- "https://gitcode.net/mirrors/wbthomason/packer.nvim",
+    install_path,
+  })
+
+  -- https://github.com/wbthomason/packer.nvim/issues/750
+  local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
+  if not string.find(vim.o.runtimepath, rtp_addition) then
+    vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
+  end
+  vim.notify("Pakcer.nvim 安装完毕")
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  vim.notify("没有安装 packer.nvim")
+  return
+end
+
+mode=require('lua/tool/kb-switch')
---
+++ lua/tool/kb-switch.lua
@@ -0,0 +1,35 @@
+local autocmd = vim.api.nvim_create_autocmd
+local mode={}
+-- 中英文切换
+mode.default_mode='com.apple.keylayout.ABC'
+mode.current_mode=''
+
+if is_mac then
+    local windowsInsertLeave = function()
+      vim.cmd(":silent :!~/.config/nvim/im-select.exe 1033")
+    end
+    autocmd("InsertLeave", {
+      group = myAutoGroup,
+      callback = windowsInsertLeave,
+    })
+else
+    local macInsertLeave = function()
+      vim.cmd(":silent :!im-select" .. " " .. mode.default_mode)
+     end
+    local macInsertEnter = function()
+      if mode.current_mode then
+        vim.cmd(":silent :!im-select" .. " " .. mode.current_mode)
+      end
+    end
+
+    autocmd("InsertLeave", {
+      group = myAutoGroup,
+      callback = macInsertLeave,
+    })
+    autocmd("InsertEnter", {
+      group = myAutoGroup,
+      callback = macInsertEnter,
+    })
+end
+-- 中英文切换
+return mode
