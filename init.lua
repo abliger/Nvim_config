899d5f12646b98ee04c11ea1abe78c927c078b4c
abliger <3330181534@qq.com>
2022-06-24 23:48:32 +0800

    add keyboard map

--- init.lua
+++ init.lua
@@ -2,12 +2,29 @@ local fn = vim.fn
 local api = vim.api
 
 is_mac = fn.has("mac") or fn.has("unix") or fn.has('linux')
+
 local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {
   clear = true,
 })
 api.nvim_command('set nu')
 api.nvim_command('set mouse=a')
 api.nvim_command('command -nargs=? OpenConfig edit ' .. fn.stdpath('config') .. '/<args>')
+
+---- map
+map = vim.api.nvim_set_keymap
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+local opt = {noremap = true, silent = true }
+map('t','<Esc>','<C-\\><C-N>',opt)
+map('t','<C-t>q','<C-\\><C-N>:q<CR>',opt)
+map("n", "<leader>t", ":set splitbelow| 5sp | set nosplitbelow | terminal<CR>", opt)
+-- 上下滚动浏览
+map("n", "<C-j>", "4j", opt)
+map("n", "<C-k>", "4k", opt)
+-- ctrl u / ctrl + d  只移动9行，默认移动半屏
+map("n", "<C-u>", "9k", opt)
+map("n", "<C-d>", "9j", opt)
+---- map
 
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local paccker_bootstrap
@@ -38,4 +55,4 @@ if not status_ok then
   return
 end
 
-mode=require('lua/tool/kb-switch')
+mode = require('tool/kb-switch')
