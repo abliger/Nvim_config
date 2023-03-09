856f5ec0b5a69c550c9428ceaeec325635bfda04
abliger <3330181534@qq.com>
2023-03-09 12:47:24 +0800

    add packer manager

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ api.nvim_command('set nu')
 api.nvim_command('set mouse=a')
 api.nvim_command('command -nargs=? OpenConfig edit ' .. fn.stdpath('config') .. '/<args>')
 
+require('lua/kbswitch')
+
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local paccker_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
@@ -33,9 +35,22 @@ end
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
 if not status_ok then
   vim.notify("没有安装 packer.nvim")
   return
 end
 
-mode=require('lua/tool/kb-switch')
+
