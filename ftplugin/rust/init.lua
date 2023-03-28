a63fe16c5aa74dbe1c314b94f72868bcb68169ba
abliger <3330181534@qq.com>
2023-03-29 01:25:15 +0800

    功能：改善 Vim 中的 Rust 支持
    - 改善 Vim 中的 Rust 开发工作流程
    - 添加功能以打开 Rust 文件的 HIR 表示
    - 创建 autocmd 以在缓冲区写入时运行 `checktime`
    - 将 `ghir` 键映射到打开 HIR 表示

--- ftplugin/rust/init.lua
+++ ftplugin/rust/init.lua
@@ -8,3 +8,12 @@ Api.nvim_create_autocmd('BufWritePost', {
 		Api.nvim_command 'checktime'
 	end,
 })
+
+local function openHir()
+	local filename = Fn.expand '%:p'
+	local rand = Fn.system 'echo $RANDOM'
+	local tempfile = '/var/folders/2x/v0w93vq55n7_vzjksyxts_kc0000gn/T/nvim.fengsixue/test' .. rand  
+	Fn.system('rustc -Z unpretty=hir ' .. filename .. ' >' .. tempfile)
+	Api.nvim_command('e ' .. tempfile)
+end
+vim.keymap.set('n', 'ghir', openHir)
